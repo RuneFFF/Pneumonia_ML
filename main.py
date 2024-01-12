cae93406635ea48a35d37fb8c836daf854d68267
Lun0x <55894484+Lun0x@users.noreply.github.com>
2024-01-12 15:53:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,42 @@
 
 import torch
 import torch.nn as nn
-import numpy as np
-import matplotlib.pyplot as plt
+import torch.optim as optim
+import torch.nn.functional as F
+#import torchvision
+#import torchvision.transforms as transforms
+#import numpy as np
+
+#In case of 3-channel images
+#class Net(nn.Module):
+#    def __init__(self):
+#        super().__init__()
+#        self.conv1 = nn.Conv2d(3, 6, 5)
+#        self.pool = nn.MaxPool2d(2, 2)
+#        self.conv2 = nn.Conv2d(6, 16, 5)
+#        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+#        self.fc2 = nn.Linear(120, 84)
+#        self.fc3 = nn.Linear(84, 10)
+#
+#    def forward(self, x):
+#        x = self.pool(F.relu(self.conv1(x)))
+#        x = self.pool(F.relu(self.conv2(x)))
+#        x = torch.flatten(x, 1) # flatten all dimensions except batch
+#        x = F.relu(self.fc1(x))
+#        x = F.relu(self.fc2(x))
+#        x = self.fc3(x)
+#        return x
+
+if __name__ == '__main__':
+  
+  net = nn.Module()
+  
+  #define parameters
+  learning_rate = 0,001
+  opt_momentum = 0,9
+  
+  #define criterion (loss) and optimizer
+  criterion = nn.CrossEntropyLoss()
+  optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=opt_momentum)
+  
+  #training loop
