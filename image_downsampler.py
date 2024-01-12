900405e38b4202ee38ee1178aec5a9a5494cfaad
RuneFFF <83013872+RuneFFF@users.noreply.github.com>
2024-01-12 22:45:00 +0100

    image_downsampler zweite Version: konvertiert in place die größe zu 750x500px, behält dabei die aspect ratio bei (durch schwarze balken)

--- image_downsampler.py
+++ image_downsampler.py
@@ -1,18 +1,30 @@
-import sys
 import os
 import numpy as np
 from PIL import Image
 import cv2
 
-def downsample(path, width, height):
-    img = cv2.imread(path)
-    downsampled = Image.fromarray(cv2.resize(img, dsize=(width, height), interpolation=cv2.INTER_CUBIC)).convert("L")
-    downsampled.save(path)
+def downsample(path, width, height, savepath):
+    img = cv2.imread(path, 0)
+    old_height, old_width = np.shape(img)
+    ratio = width/height
 
-downsample('test_image.jpeg', 750, 500)
-'''
+
+    if old_width/old_height > ratio:
+        bar_size = round(((old_width / ratio) - old_height)/2)
+        bar = np.zeros((bar_size, old_width))
+        barred = np.row_stack((bar, img, bar))
+    elif old_width/old_height < ratio:
+        bar_size = round(((old_height * ratio) - old_width)/2)
+        bar = np.zeros((old_height, bar_size))
+        barred = np.column_stack((bar, img, bar))
+    else:
+        barred = img
+
+    final = (cv2.resize(barred, dsize=(width, height), interpolation=cv2.INTER_CUBIC))
+    Image.fromarray(final).convert('L').save(savepath)
+
+#downsample('IM-0073-0001.jpeg', 750, 500, 'img.jpeg')
 for root,_,files in os.walk('../chest_xray'):
     for filename in files:
         if filename[-5:] == ('.jpeg'):
-            downsample(os.path.join(root,filename), 750, 500)
-'''
+            downsample(os.path.join(root,filename), 750, 500, os.path.join(root,filename))
