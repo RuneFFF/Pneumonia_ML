5821be9dfe8d9151c1f5473771421935bdab136b
RuneFFF <83013872+RuneFFF@users.noreply.github.com>
2024-01-18 13:17:25 +0100

    csv jetzt mit zufälliger andordnung der dateien

--- create_XR_csv.py
+++ create_XR_csv.py
@@ -1,14 +1,14 @@
 import csv
 import os
-#import pandas as pd
+import random
 
 def makeCSV(path):
     with open('chest_xray_data.csv','w',newline='') as file:
         writer = csv.writer(file)
         headerList = ['feature', 'name', 'label']
         writer.writerow(headerList)
-        for dir, _, files in os.walk(path):
-            for f in files:
+        dir = os.listdir(path)
+        for f in random.sample(dir, len(dir)):
                 if f[-5:] == ('.jpeg'):
                     #get feature
                     if 'n' in f:
@@ -22,9 +22,5 @@ def makeCSV(path):
                             label = 'V'
                     file_name = f.split('.')[0]
                     writer.writerow([feature, file_name, label])
-    # #add header
-    # headerList = ['type', 'feature', 'name']  # type=test/train/val;feature=NORMAL/PNEUMONIA, name=Image name
-    # file = pd.read_csv('chest_xray_data.csv')
-    # file.to_csv('chest_xray_data.csv', header=headerList, index=True)
 
 makeCSV('../chest_xray')
